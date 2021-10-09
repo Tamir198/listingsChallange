bf79c011db26225a587d856b4ea64775ec03ae00
Tamir <1tamir198@gmail.com>
2021-10-10 00:10:10 +0300

    UI update

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -3,9 +3,7 @@ import ListItem from './ListItem'
 const ListComponenet = (props) =>{
     return (
     <div>  
-      {/* TODO init list items with job object (to creat it dymanic) */}
-      {props.data.map(job => <div>{job.languages}</div>)}
-      <ListItem/>
+      {props.data.map(job => <ListItem key={job.id} jobData={job}>{job.languages}</ListItem>)}
     </div>
     )
 
