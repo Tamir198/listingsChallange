c0c03939bd1c0e6dec0db09ed8b4429540602665
Tamir <1tamir198@gmail.com>
2021-10-09 13:13:12 +0300

    Finished Crud methods

--- server.js
+++ server.js
@@ -3,27 +3,17 @@ const { port } = require('./config.json');
 const data = require('./DummyData');
 
 const app = express();
-
-// app.get('/example', (req, res) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.json({ message: 'Hi, from the server' })
-// });
+var bodyParser = require('body-parser')
+const jsonParser = bodyParser.json();
 
 
 app.get('/data', (req, res) => {
   res.json(data);
 });
 
-app.post('/:newItem', (req, res) => {
-  //TODO add new item to the list
-  //data.push(req.params.newItem);
-  res.json({ message: 'Hi,post from the server' + req.params.newItem })
-});
-
-
-app.put('/', (req, res) => {
-  res.json({ message: 'Hi,put from the server' })
-
+app.post('/data', jsonParser, (req, res) => {
+  data.push(req.body);
+  res.json({ message: "Item has beed aded", fullItems: data})
 });
 
 app.delete('/data/:id', (req, res) => {
@@ -33,18 +23,4 @@ app.delete('/data/:id', (req, res) => {
 
 app.listen(port, () => console.log(`server is listening on: ${port}`));
 
-//Non relevant, amnon asked for us to filter on the client
-// app.get('/data/:filterParam/', (req, res) => {
-//   const companyName = req.params.filterParam;
-//   const finalData = [];
 
-//   for(key in data) {  
-//      if (data[key].listedCompany == companyName) finalData.push(data[key]); 
-//     }
-
-//   res.json({ 
-//     // message: data[0].listedCompany,
-//     message: finalData,
-
-//   })
-// });
