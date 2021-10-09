23d1342760e25812740d8a739f0b468059ec60d7
Tamir <1tamir198@gmail.com>
2021-10-09 13:32:38 +0300

    Adding example html UI

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,24 +1,27 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import FilterBox from '../Filter/FilterBox';
+import List from '../List/List';
 
 function App() {
   const [exampleRes, setExampleRes] = useState();
 
-  const fetchData = async () => {
-    try {
-      const { message } = await axios.get('http://localhost:8080/example').then(({ data }) => data);
-      setExampleRes(message);
-    } catch (err) { console.log(err); }
-  };
+  // const fetchData = async () => {
+  //   try {
+  //     const { message } = await axios.get('http://localhost:8080/example').then(({ data }) => data);
+  //     setExampleRes(message);
+  //   } catch (err) { console.log(err); }
+  // };
 
-  useEffect(() => { fetchData(); });
+  // useEffect(() => { fetchData(); });
 
   return (
     <div className="App">
       <header />
       <div className="container">
-        <h1> {exampleRes} </h1>
+        <FilterBox></FilterBox>
+        <List></List>
       </div>
     </div>
   );
---
+++ client/src/components/Filter/FilterBox.js
@@ -0,0 +1,11 @@
+const FilterBox = (props) => {
+  return (
+      <form action="">
+      <label for="filter">filter:</label>
+      <input type="text" name="filter" className="filter" placeholder="Enter your filter here"></input>
+      <br></br>
+      </form>
+  )
+}
+
+export default FilterBox;
