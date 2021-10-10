878de690aa3e78b8a0ccdc1ce480892ca95dcd98
Tamir <1tamir198@gmail.com>
2021-10-10 18:10:45 +0300

    Adding state hook to List.js

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -39,6 +39,7 @@ function App() {
   };*/
 
   const [jobs, setJobs] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
   //Access jobs with jobs.data
   const getAllData = async () => {
--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react';
+
 import ListItem from './ListItem'
 
-const ListComponenet = (props) =>{
-    return (
-    <div>  
-      {props.data.map(job => <ListItem deleteItem={props.deleteItem}key={job.id} jobData={job}>{job.languages}</ListItem>)}
+const ListComponenet = (props) => {
+ 
+    const [jobList, setJobList] = useState(props.data);
+
+    useEffect(() => {
+      setJobList(props.data)
+      console.log(jobList)
+    }, []);
+
+  
+  return (
+    <div>
+      {
+        props.data.map(
+          job => <ListItem
+            deleteItem={props.deleteItem}
+            key={job.id}
+            jobData={job}>{job.languages}
+          </ListItem>)
+      }
     </div>
-    )
+  )
 
 }
 
